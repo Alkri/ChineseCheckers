879882590dc39ea0cf1b9b8e89533a32d193ddc2
Alkri <alkriiiiii@gmail.com>
2022-12-06 22:05:38 +0800

    实现了动画效果、音效，加入了智障AI。

---
+++ AIGameBoard.cpp
@@ -0,0 +1,6 @@
+#include "AIGameBoard.h"
+
+AIGameBoard::AIGameBoard(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ AIGameBoard.h
@@ -0,0 +1,16 @@
+#ifndef AIGAMEBOARD_H
+#define AIGAMEBOARD_H
+
+#include <QWidget>
+
+class AIGameBoard : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit AIGameBoard(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // AIGAMEBOARD_H
